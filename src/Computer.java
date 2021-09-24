0488e6ff0a98c4373f8d5eab18f503d772d315e7
Tanzia Ahmed <52648080+tanzia-ahmed@users.noreply.github.com>
2021-09-24 14:24:46 -0400

    computer class constructors made more efficienct and driver class a,b,c completed

--- src/Computer.java
+++ src/Computer.java
@@ -6,15 +6,6 @@ public class Computer {
     double price;
     protected static int computerCount = 0;
 
-    public Computer() {
-
-        System.out.println("Creating Computer using default constructor");
-
-        brand = "Apple"; model = "MacBook Pro1"; SN = 1; price = 1500;
-
-        computerCount ++;
-    }
-
     public Computer(String brandValue, String modelValue, long SNValue, double priceValue ) {
 
         System.out.println("Creating Computer using parametrized constructor");
@@ -27,16 +18,16 @@ public class Computer {
         computerCount ++;
     }
 
-    public Computer(Computer c) {
+    public Computer() {
 
+        this("Apple", "MacBook Pro1", 1, 1500 );
+        System.out.println("Creating Computer using default constructor");
+    }
+
+    public Computer(Computer c) {
+        this(c.brand, c.model, c.SN, c.price);
         System.out.println("Creating Computer using copy constructor");
 
-        brand = c.brand;
-        model = c.model;
-        SN = c.SN;
-        price = c.price;
-
-        computerCount ++;
     }
 
     public String getBrand() {
@@ -106,7 +97,7 @@ public class Computer {
 
         Computer c1 = new Computer();
         Computer c2 = new Computer("Lenovo", "Thinkpad", 2, 1000);
-        Computer c3 = new Computer(c2);
+        Computer c3 = new Computer(c1);
         Computer c4 = new Computer();
         c4.setBrand("Dell"); c4.setModel("Inspiron"); c4.setSN(4); c4.setPrice(1200);
 
--- src/Driver.java
+++ src/Driver.java
@@ -1,2 +1,46 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Driver {
+
+    private static String password = "password";
+
+    public static void main (String [] args){
+        // WELCOME MESSAGE
+        System.out.println("Welcome to the computer tracking app!");
+        System.out.println();
+        System.out.println("How many computers (at maximum) can be stored in your computer store?");
+
+        Scanner scanner =  new Scanner(System.in);
+
+        //PROMPT
+        int maxComputers = scanner.nextInt(); //max number of computers
+        ArrayList<Computer> inventory = new ArrayList<>();
+
+        int option = 0;
+        while(option !=5){
+            System.out.println("What do you want to do?\n" +
+                    "1. Enter new computers (password required)\n" +
+                    "2. Change information of a computer (password required)\n" +
+                    "3. Display all computers by a specific brand\n" +
+                    "4. Display all computers under a certain a price.\n" +
+                    "5. Quit\n" +
+                    "Please enter your choice >");
+            //PROMPT
+            option = scanner.nextInt();
+
+            switch (option){
+                case 1:
+                    System.out.print("Password: ");
+                    for(int i=0; i<3; i++) {
+                        String givenPassword = scanner.next();
+                        if(givenPassword == password){
+                            Computer newComputer = new Computer();
+                        }
+                    }
+            }
+        }
+
+
+    }
 }
