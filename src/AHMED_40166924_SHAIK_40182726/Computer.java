bd33a2be783ab686d3bd23f67f3fdcb04f96cc6d
Tanzia Ahmed <52648080+tanzia-ahmed@users.noreply.github.com>
2021-10-07 18:13:46 -0400

    comments and try-catch for input mismatch cases added

--- src/AHMED_40166924_SHAIK_40182726/Computer.java
+++ src/AHMED_40166924_SHAIK_40182726/Computer.java
@@ -1,16 +1,35 @@
+// -----------------------------------------------------
+// Assignment 1
+// © Tanzia Ahmed
+// © Abdul Shaik
+// Written by: 40166924 - Tanzia Ahmed
+//             40182726 - Abdul Shaik
+// -----------------------------------------------------
+/*
+        This program has a Computer class with the required specifications (attributes, accessors and mutators,
+        constructors) and a Driver class which has a various methods for different functions a computer store requires.
+ */
 package AHMED_40166924_SHAIK_40182726;
+/**
+ * Computer Class
+ * Atrributes : brand, model, Serial Number, price, computer count
+ * @author 40182726 - SHAIK
+ */
 public class Computer {
 
-    /**
-     *
-     * @author 40182726 - SHAIK
-     */
     String brand;
     String model;
     long SN;
     double price;
     protected static int computerCount = 0;
 
+    /**
+     * Parametrized Constructor for Computer
+     * @param brandValue brand
+     * @param modelValue model
+     * @param SNValue Serial Number
+     * @param priceValue Price
+     */
     public Computer(String brandValue, String modelValue, long SNValue, double priceValue ) {
 
         brand = brandValue;
@@ -21,52 +40,99 @@ public class Computer {
         computerCount ++;
     }
 
+    /**
+     * default constructor
+     */
     public Computer() {
 
         this("Apple", "MacBook Pro1", 1, 1500 );
     }
 
+    /**
+     * Copy Constructor
+     * @param c computer object to copy
+     */
     public Computer(Computer c) {
         this(c.brand, c.model, c.SN, c.price);
 
     }
 
+    /**
+     * Getter for brand
+     * @return brand
+     */
     public String getBrand() {
         return brand;
     }
 
+    /**
+     * Setter for brand
+     * @param brand brand
+     */
     public void setBrand(String brand) {
         this.brand = brand;
     }
 
+    /**
+     * Getter for model
+     * @return model
+     */
     public String getModel() {
         return model;
     }
 
+    /**
+     * Setter for model
+     * @param model model
+     */
     public void setModel(String model) {
         this.model = model;
     }
 
+    /**
+     * Getter for Sn
+     * @return SN
+     */
     public long getSN() {
         return SN;
     }
 
+    /**
+     * Setter for SN
+     * @param SN Sn
+     */
     public void setSN(long SN) {
         this.SN = SN;
     }
 
+    /**
+     * Getter for price
+     * @return price
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     * Setter for price
+     * @param price price
+     */
     public void setPrice(double price) {
         this.price = price;
     }
 
+    /**
+     * Static method to find number of computers at any time
+     * @return number of computers
+     */
      static int findNumberOfCreatedComputers() {
         return computerCount;
     }
 
+    /**
+     * To string method to display computer information
+     * @return Computer information
+     */
     @Override
     public String toString() {
         return  "Brand :" + " "+ brand + "\n" +
@@ -75,11 +141,20 @@ public class Computer {
                 "Price :" + " $" + price;
     }
 
+    /**
+     * Method to show information of a particular computer
+     * @param c computer object
+     */
     public static void showComputerInfo(Computer c) {
 
         System.out.println(c);
     }
 
+    /**
+     * Method to check if two computers are same
+     * @param x Any Object
+     * @return True if Object is the same computer else False
+     */
     public boolean equals(Object x) {
         if (x == null || this.getClass() != x.getClass())
             return false;
--- src/AHMED_40166924_SHAIK_40182726/Driver.java
+++ src/AHMED_40166924_SHAIK_40182726/Driver.java
@@ -1,19 +1,31 @@
+// -----------------------------------------------------
+// Assignment 1
+// © Tanzia Ahmed
+// © Abdul Shaik
+// Written by: 40166924 - Tanzia Ahmed
+//             40182726 - Abdul Shaik
+// -----------------------------------------------------
+/*
+        This program has a Computer class with the required specifications (attributes, accessors and mutators,
+        constructors) and a Driver class which has a various methods for different functions a computer store requires.
+ */
 package AHMED_40166924_SHAIK_40182726;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import static AHMED_40166924_SHAIK_40182726.Computer.findNumberOfCreatedComputers;
-
+/**
+ * Driver Class
+ */
 public class Driver {
 
     private static String password;
-    private static Scanner scanner1;
-    private static Scanner scanner2;
     private static int maxComputers; //max number of computers that can be added
     private static Computer[] inventory; //array of computers
 
     /**
-     *
+     * Main method
      * @author 40166924 - AHMED
      */
     public static void main(String[] args) {
@@ -23,47 +35,72 @@ public class Driver {
         // WELCOME MESSAGE
         System.out.println("Welcome to the computer tracking app!");
         System.out.println();
-        System.out.println("How many computers (at maximum) can be stored in your computer store?");
 
-        scanner1 = new Scanner(System.in);
-        scanner2 = new Scanner(System.in);
+        Scanner s=null;
 
-        try {
-            //PROMPT
-            maxComputers = scanner1.nextInt();
-            inventory = new Computer[maxComputers];
-
-            Menu();
-        } catch (Exception e) {
-            System.out.println(e);
+        boolean matchedInputType = false;
+        while(matchedInputType==false) { //prompting until input type matched--expected integer type
+            try {
+                System.out.println("How many computers (at maximum) can be stored in your computer store?");
+                //PROMPT
+                s=new Scanner(System.in);
+                maxComputers = s.nextInt();
+                matchedInputType = true;
+            }catch (InputMismatchException e) {
+                matchedInputType = false;
+                System.out.println(e);
+                System.out.println();
+            }
+            catch (Exception e) {
+                System.out.println(e);
+                break;
+            }
         }
+
+        inventory = new Computer[maxComputers];
+        Menu();
 
     }
 
     /**
-     *
+     * Main Menu, various cases for different methods handled
      * @author 40166924 - AHMED
      */
     private static void Menu() {
         int option = 0; //user input value
         while (option != 5) {
-            DisplayMenuOptions();
-
-            //PROMPT
-            option = scanner1.nextInt();
+            Scanner s=null;
+            boolean matchedInputType = false;
+            while(matchedInputType==false) { //prompting until input type matched--expected integer type
+                try {
+                    //PROMPT
+                    DisplayMenuOptions();
+                    s=new Scanner(System.in);
+                    option = s.nextInt();
+                    matchedInputType = true;
+                } catch (InputMismatchException e) {
+                    matchedInputType = false;
+                    System.out.println(e);
+                    System.out.println();
+                } catch (Exception e) {
+                    System.out.println(e);
+                    break;
+                }
+            }
             switch (option) {
                 //1. ENTER NEW COMPUTERS
                 case 1:
                     System.out.print("Enter Password: ");
-                    for (int i = 0; i < 3; i++) {
+                    for (int i = 0; i < 3; i++) { //check password 3 times
+                        Scanner scanner = new Scanner(System.in);
                         //PROMPT
-                        String givenPassword = scanner1.next();
+                        String givenPassword = scanner.next();
                         //CHECKING PASSWORD
                         boolean pwPassed = checkPassword(givenPassword);
                         if (pwPassed) { //MATCHED
                             EnterNewComputers();
                             break;
-                        } else {
+                        } else { //DIDN'T MATCH
                             System.out.println("Oops!! Wrong Password!");
                             System.out.println();
                             if (i < 2)
@@ -75,15 +112,16 @@ public class Driver {
                 //2. UPDATE INFORMATION OF A COMPUTER
                 case 2:
                     System.out.print("Enter Password: ");
-                    for (int i = 0; i < 3; i++) {
+                    for (int i = 0; i < 3; i++) { //check password 3 times
+                        Scanner scanner = new Scanner(System.in);
                         //PROMPT
-                        String givenPassword = scanner1.next();
+                        String givenPassword = scanner.next();
                         //CHECKING PASSWORD
                         boolean pwPassed = checkPassword(givenPassword);
                         if (pwPassed) { //MATCHED
                             UpdateComputer();
                             break;
-                        } else {
+                        } else { //DIDN'T MATCH
                             System.out.println("Oops!! Wrong Password!");
                             System.out.println();
                             if (i < 2)
@@ -95,14 +133,34 @@ public class Driver {
                 //3. SEARCH BY BRAND
                 case 3:
                     System.out.println("Enter the brand name you want to search:");
+                    Scanner scanner = new Scanner(System.in);
                     //PROMPT
-                    String brand = scanner2.next();
+                    String brand = scanner.next();
                     findComputersBy(brand);
                     break;
                 //4. SEARCH BY PRICE
                 case 4:
-                    System.out.println("Enter price to see all computers within the price :");
-                    double pricelessthan = scanner2.nextDouble();
+                    Scanner s2=null;
+                    boolean matchedInputType2 = false;
+                    double pricelessthan = 0;
+                    while(matchedInputType2==false) { //prompting until input type matched--expected double type
+                        try {
+                            //PROMPT
+                            System.out.println("Enter price to see all computers within the price :");
+                            s2=new Scanner(System.in);
+                            pricelessthan = s2.nextDouble();
+                            matchedInputType = true;
+                            break;
+
+                        } catch (InputMismatchException e) {
+                            matchedInputType = false;
+                            System.out.println(e);
+                            System.out.println();
+                        } catch (Exception e) {
+                            System.out.println(e);
+                            break;
+                        }
+                    }
                     findCheaperThan(pricelessthan);
                     break;
                 //5. QUIT
@@ -115,7 +173,7 @@ public class Driver {
     }
 
     /**
-     *
+     * Display main menu options
      * @author 40166924 - AHMED
      */
     private static void DisplayMenuOptions() {
@@ -129,7 +187,9 @@ public class Driver {
     }
 
     /**
-     *
+     *  Method to check if password is correct
+     *  @param givenPassword password entered
+     *  @return True if match else false
      * @author 40166924 - AHMED
      */
     private static boolean checkPassword(String givenPassword) {
@@ -143,16 +203,33 @@ public class Driver {
     }
 
     /**
-     *
+     * To enter new computer
      * @author 40166924 - AHMED
      */
     private static void EnterNewComputers() {
         if (findNumberOfCreatedComputers() == maxComputers) {
             System.out.println("HOUSEFULL!!");
         } else {
-            System.out.println("How many computers do you want to enter?");
-            //PROMPT
-            int numComp = scanner1.nextInt();
+            Scanner s=null;
+            int numComp = 0;
+            boolean matchedInputType = false;
+            while(matchedInputType==false) { //prompting until input type matched--expected integer type
+                try {
+                    System.out.println("How many computers do you want to enter?");
+                    //PROMPT
+                    s = new Scanner(System.in);
+                    numComp = s.nextInt();
+                    matchedInputType = true;
+                }catch (InputMismatchException e) {
+                    matchedInputType = false;
+                    System.out.println(e);
+                    System.out.println();
+                }
+                catch (Exception e) {
+                    System.out.println(e);
+                    break;
+                }
+            }
             //CHECKING FOR AVAILABLE CAPACITY
             if (numComp <= (maxComputers - findNumberOfCreatedComputers())) {
                 //CREATING NEW COMPUTERS
@@ -169,24 +246,57 @@ public class Driver {
     }
 
     /**
-     *
+     * To update any computer
      * @author 40166924 - AHMED
      */
     private static void UpdateComputer() {
-        System.out.println("What computer number do you want to update?");
+        Scanner s=null;
+        int compIndex = 0;
+        boolean matchedInputType = false;
+        while(matchedInputType==false) { //prompting until input type matched--expected integer type
+            try {
+                System.out.println("What computer number do you want to update?");
+                s = new Scanner(System.in);
+                //PROMPT
+                compIndex = s.nextInt();
+                matchedInputType = true;
+            }catch (InputMismatchException e) {
+                matchedInputType = false;
+                System.out.println(e);
+                System.out.println();
+            }
+            catch (Exception e) {
+                System.out.println(e);
+                break;
+            }
+        }
 
-        //PROMPT
-        int compIndex = scanner1.nextInt();
-
-        if (compIndex < findNumberOfCreatedComputers() && compIndex > -1) {
+        if (compIndex < findNumberOfCreatedComputers() && compIndex > -1) { //if computer index is valid
             System.out.println("Computer # " + compIndex + "\n" +
                     inventory[compIndex].toString());
             UpdateMenu(inventory[compIndex]);
-        } else {
-            System.out.println("Sorry, this number is not registered with a computer.\n" +
-                    "Do you want to enter a new computer (1) OR go back (0)?");
-            //PROMPT
-            int op = scanner1.nextInt();
+        } else { //if computer index is invalid
+            System.out.println("Sorry, this number is not registered with a computer.\n" );
+            Scanner s2=null;
+            boolean matchedInputType2 = false;
+            int op = 0;
+            while(matchedInputType2==false) { //prompting until input type matched--expected integer type
+                try {
+                    System.out.println("Do you want to enter a new computer (1) OR go back (0)?");
+                    s2 = new Scanner(System.in);
+                    //PROMPT
+                    op = s2.nextInt();
+                    matchedInputType2 = true;
+                } catch (InputMismatchException e) {
+                    matchedInputType2 = false;
+                    System.out.println(e);
+                    System.out.println();
+                } catch (Exception e) {
+                    System.out.println(e);
+                    break;
+                }
+            }
+
             switch (op) {
                 //1. ENTER NEW COMPUTER
                 case 1:
@@ -200,48 +310,57 @@ public class Driver {
     }
 
     /**
-     *
+     * Updates details of a computer
      * @author 40166924 - AHMED
      */
     private static void UpdateMenu(Computer computer) {
         int option = 0; //user input value
         while (option != 5) {
-            DisplayUpdateMenuOptions();
-
-            //PROMPT
-            option = scanner2.nextInt();
+            Scanner s=null;
+            boolean matchedInputType = false;
+            while(matchedInputType==false) { //prompting until input type matched--expected integer type
+                try {
+                    DisplayUpdateMenuOptions();
+                    s = new Scanner(System.in);
+                    //PROMPT
+                    option = s.nextInt();
+                    matchedInputType = true;
+                    break;
+                }catch (InputMismatchException e) {
+                    matchedInputType = false;
+                    System.out.println(e);
+                    System.out.println();
+                } catch (Exception e) {
+                    System.out.println(e);
+                    break;
+                }
+            }
             switch (option) {
                 case 1:
                     //BRAND
-                    System.out.println("Change brand name " + computer.getBrand() + " to:");
                     UpdateBrand(computer);
-                    System.out.println(computer);
                     break;
                 case 2:
                     //MODEL
-                    System.out.println("Change model name " + computer.getModel() + " to:");
                     UpdateModel(computer);
-                    System.out.println(computer);
                     break;
                 case 3:
                     //SN
-                    System.out.println("Change serial number " + computer.getSN() + " to:");
                     UpdateSN(computer);
                     break;
                 case 4:
                     //PRICE
-                    System.out.println("Change price " + computer.getPrice() + " to:");
                     UpdatePrice(computer);
                     break;
                 case 5:
                     //QUIT
-                    return;
+                    return; // returns to main menu
             }
         }
     }
 
     /**
-     *
+     * Display update menu options
      * @author 40166924 - AHMED
      */
     private static void DisplayUpdateMenuOptions() {
@@ -255,51 +374,96 @@ public class Driver {
     }
 
     /**
-     *
+     * Updates brand
+     * @param computer object
      * @author 40166924 - AHMED
      */
     private static void UpdateBrand(Computer computer) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Change brand name " + computer.getBrand() + " to:");
         //PROMPT
         String brand = sc.nextLine();
         computer.setBrand(brand);
+        System.out.println(computer);
     }
 
     /**
-     *
+     * Updates model
+     * @param computer object
      * @author 40166924 - AHMED
      */
     private static void UpdateModel(Computer computer) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Change model name " + computer.getModel() + " to:");
         //PROMPT
         String model = sc.nextLine();
         computer.setModel(model);
+        System.out.println(computer);
     }
 
     /**
-     *
+     * Updates Serial Number (SN)
+     * @param computer object
      * @author 40166924 - AHMED
      */
     private static void UpdateSN(Computer computer) {
-        //PROMPT
-        Long sn = scanner2.nextLong();
+        Scanner s=null;
+        Long sn = Long.valueOf(0);
+        boolean matchedInputType = false;
+        while(matchedInputType==false) { //prompting until input type matched--expected long type
+            try {
+                System.out.println("Change serial number " + computer.getSN() + " to:");
+                s=new Scanner(System.in);
+                //PROMPT
+                sn = s.nextLong();
+                matchedInputType = true;
+            }catch (InputMismatchException e) {
+                matchedInputType = false;
+                System.out.println(e);
+                System.out.println();
+            } catch (Exception e) {
+                System.out.println(e);
+                break;
+            }
+        }
+
         computer.setSN(sn);
         System.out.println(computer);
     }
 
     /**
-     *
+     * Updates price
+     * @param computer object
      * @author 40166924 - AHMED
      */
     private static void UpdatePrice(Computer computer) {
-        //PROMPT
-        double price = scanner2.nextDouble();
+        Scanner s=null;
+        double price = 0;
+        boolean matchedInputType = false;
+        while(matchedInputType==false) { //prompting until input type matched--expected double type
+            try {
+                System.out.println("Change price " + computer.getPrice() + " to:");
+                s=new Scanner(System.in);
+                //PROMPT
+                price = s.nextDouble();
+                matchedInputType = true;
+            }catch (InputMismatchException e) {
+                matchedInputType = false;
+                System.out.println(e);
+                System.out.println();
+            } catch (Exception e) {
+                System.out.println(e);
+                break;
+            }
+        }
+
         computer.setPrice(price);
         System.out.println(computer);
     }
 
     /**
-     *
+     * To search computers by brand name
+     * @param brand
      * @author 40166924 - AHMED
      */
     private static void findComputersBy(String brand) {
@@ -316,7 +480,9 @@ public class Driver {
 
     }
     /**
-     *
+     * To search computers by price
+     * displays computers having price less than the value given
+     *  @param pricelessthan
      * @author 40182726 - SHAIK
      * edited by: 40166924 - AHMED
      */
